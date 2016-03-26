c59d31f642ce5f259d6546af5dfd2506faf03fef
Jay Jamieson <jacques@jaytechsupport.com>
2016-03-26 18:51:45 +1300

    minor edits

--- JavaScript/Singleton.js
+++ JavaScript/Singleton.js
@@ -2,6 +2,8 @@
  * Created by Jay on 23/02/2016.
  */
 
+
+/**Singleton*/
 let Router = (function () {
     "use strict";
     let instance; //prevent modification of "instance" variable by specifying it within the IIFE scope.
@@ -19,14 +21,13 @@ let Router = (function () {
     //This is the same as specifying this.getInstance = function(){} and can be used as an alternative.
     Singleton.getInstance = function () {
         return instance || new Singleton();
-    }
+    };
     return Singleton; //after initialization is complete we return the singleton.
 })();
 
 let a = new Router(); //Try and create two instances
-let b = new Router();
+let b = Router();
 
 a.addUser("James"); //Try and modify the instances separately and notice that they hold the same data
 b.addUser("Sarah");
 console.log(a.users + " " + b.users);
-
---
+++ JavaScript/factory.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Jay on 25/02/2016.
+ */
+
+/**Car Factory*/
